ca9e277b783a680a622b6def0dd7fc610c372f4c
NangmanCo <admin@nangman.io>
2023-03-30 14:23:07 +0900

    tail wind apply

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1 className="text-3xl font-bold underline">Hello React</h1>
       <h2>My name is asdf</h2>
       <h3>My Favorite drink is Zero Coke 👑</h3>
     </div>
